d55737b91ba10e13cf979e295dffd2ff69f6c644
yirman <germanguevara2496@gmail.com>
2022-12-06 00:24:00 -0400

    using criptodolar rest api

--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
-	"regexp"
 
-	"github.com/gocolly/colly"
 	"github.com/gorilla/mux"
 )
 
@@ -28,7 +26,7 @@ func main() {
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", index)
-	r.HandleFunc("/rates", queryRates).Methods("GET")
+	r.HandleFunc("/rates", queryRates).Methods("GET").Queries("type", "{type}").Queries("base", "{base}")
 	http.Handle("/", r)
 
 	port := os.Getenv("PORT")
@@ -44,31 +42,53 @@ func index(w http.ResponseWriter, r *http.Request) {
 }
 
 func queryRates(w http.ResponseWriter, r *http.Request) {
-	// vars := mux.Vars(r)
 
-	c := colly.NewCollector()
+	r.ParseForm()
+	currencyType := r.Form.Get("type")
+	currencyBase := r.Form.Get("base")
 
-	c.OnHTML("section.tgme_channel_history", func(e *colly.HTMLElement) {
+	response, err := http.Get("https://exchange.vcoud.com/coins/latest?type=" + currencyType + "&base=" + currencyBase)
 
-		var rates Rates
-		e.ForEach("div.tgme_widget_message_wrap", func(_ int, f *colly.HTMLElement) {
-			message := f.DOM.Find("div.tgme_widget_message_text").Text()
-			match1, _ := regexp.MatchString(rateRegex, message)
-			match2, _ := regexp.MatchString(dateHourRateRegex, message)
-			if match1 || match2 {
-				rates = append(rates, Rate{Data: message})
-				fmt.Println(message + "\n" + "--------------------------------------------")
-			}
-		})
+	if err != nil {
+		fmt.Print(err.Error())
+	}
 
-		jsonResponse, err := json.Marshal(rates)
-		if err != nil {
-			return
-		}
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
 
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write(jsonResponse)
-	})
-	c.Visit("https://t.me/s/enparalelovzlatelegram")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(responseData)
 }
+
+// func queryRates(w http.ResponseWriter, r *http.Request) {
+// 	// vars := mux.Vars(r)
+
+// 	c := colly.NewCollector()
+
+// 	c.OnHTML("section.tgme_channel_history", func(e *colly.HTMLElement) {
+
+// 		var rates Rates
+// 		e.ForEach("div.tgme_widget_message_wrap", func(_ int, f *colly.HTMLElement) {
+// 			message := f.DOM.Find("div.tgme_widget_message_text").Text()
+// 			match1, _ := regexp.MatchString(rateRegex, message)
+// 			match2, _ := regexp.MatchString(dateHourRateRegex, message)
+// 			if match1 || match2 {
+// 				rates = append(rates, Rate{Data: message})
+// 				fmt.Println(message + "\n" + "--------------------------------------------")
+// 			}
+// 		})
+
+// 		jsonResponse, err := json.Marshal(rates)
+// 		if err != nil {
+// 			return
+// 		}
+
+// 		w.Header().Set("Content-Type", "application/json")
+// 		w.WriteHeader(http.StatusOK)
+// 		w.Write(jsonResponse)
+// 	})
+// 	c.Visit("https://t.me/s/enparalelovzlatelegram")
+// }
